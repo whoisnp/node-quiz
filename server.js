548f7bc7882f1ab7c145da45c06dc671cb14a23f
Nithya-Prakash <nithyap322@gmail.com>
2020-09-29 13:40:50 +0530

    Changed index and server files

--- server.js
+++ server.js
@@ -1,31 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const path = require('path')
-const MongoClient = require('mongodb').MongoClient;
-var ObjectID = require('mongodb').ObjectID
+const path = require("path");
+const MongoClient = require("mongodb").MongoClient;
+var ObjectID = require("mongodb").ObjectID;
 
-app.set('view engine','ejs')
-app.use(bodyParser.urlencoded({ extended: true }))
-
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 //* mongoDB connect
-const connectionString = "mongodb+srv://whoisNP:NithyaPrakash123@cluster0-gihtk.mongodb.net/test?retryWrites=true&w=majority"
-MongoClient.connect(connectionString,{ useUnifiedTopology: true })
-.then(client => {
-    console.log('Connected to Database')
-    const db = client.db('Quiz-App')
-    const questCollection = db.collection('question') 
-    
+const connectionString =
+  "mongodb+srv://whoisNP:NithyaPrakash123@cluster0-gihtk.mongodb.net/test?retryWrites=true&w=majority";
+MongoClient.connect(connectionString, { useUnifiedTopology: true })
+  .then((client) => {
+    console.log("Connected to Database");
+    const db = client.db("Quiz-App");
+    const questCollection = db.collection("question");
+
     // ! POST REQUEST
-    app.post('/add',(req,res)=>{
-        questCollection.insertOne(req.body)
-        .then(result => {
-            // res.render('index.ejs',{ question:results })
-            res.redirect('/')
-            console.log(result)
-        }).catch(error => console.error(error))
-    })
+    app.post("/add", (req, res) => {
+      questCollection
+        .insertOne(req.body)
+        .then((result) => {
+          // res.render('index.ejs',{ question:results })
+          res.redirect("/");
+          console.log(result);
+        })
+        .catch((error) => console.error(error));
+    });
 
     // app.post('/edit',(req,res)=>{
     //     questCollection.updateMany(req.body)
@@ -51,65 +53,59 @@ MongoClient.connect(connectionString,{ useUnifiedTopology: true })
     //     })
     // })
 
-
-
-
     // ! GET REQUEST
-    app.get('/',(req,res)=>{
-        res.send('hello world')
-    })
-    app.get('/add',(req,res)=>{
-        res.render('index.ejs')
-    })
-    app.get('/game',(req,res)=>{
-        res.sendFile('views/game.html', {root: __dirname })
-    })
-    app.get('/preview',(req,res)=>{
-        db.collection('question').find().toArray()
-        .then(results => {
-            res.render('preview.ejs',{ question:results })
-            // console.log(results)
-        }).catch(error => console.error(error))
-    })
-    app.get('/edit/:id',(req,res)=>{
-        var id = req.params.id
-        var o_id = new ObjectID(id)
-        db.collection('question').find(id).toArray()
-        .then(results => {
-            res.render('update.ejs',{ question:results,id})
-        }).catch(error => console.error(error))
-    })
-    // app.get('/edit/:id',(req,res)=>{
-    //     var id = req.params.id
-    //     var o_id = new ObjectID(id)
-    //     db.collection('question').find({_id:o_id}).toArray()
-    //     .then(results => {
-    //         res.render('update.ejs',{ question:results})
-    //     }).catch(error => console.error(error))
-    // })
+    app.get("/", (req, res) => {
+      res.send("hello world");
+    });
+    app.get("/add", (req, res) => {
+      res.render("index.ejs");
+    });
+    app.get("/game", (req, res) => {
+      res.sendFile("views/game.html", { root: __dirname });
+    });
+    app.get("/preview", (req, res) => {
+      db.collection("question")
+        .find()
+        .toArray()
+        .then((results) => {
+          res.render("preview.ejs", { question: results });
+          // console.log(results)
+        })
+        .catch((error) => console.error(error));
+    });
+    app.get("/edit/:id", (req, res) => {
+      var id = req.params.id;
+      var o_id = new ObjectID(id);
+      db.collection("question")
+        .find(id)
+        .toArray()
+        .then((results) => {
+          res.render("update.ejs", { question: results, id });
+        })
+        .catch((error) => console.error(error));
+    });
 
     // ! PUT REQUEST
-    app.put('/edit/:id',(req,res)=>{
-        var oldValue =     {
-            _id: 'req.param.id'
-        } 
-        db.collection('question').updateMany(oldValue,req.body)
-        .then(results => {
-            id = req.params.id
-            res.render('update.ejs',{ question:results})
-        }).catch(error => console.error(error))
-    })
+    app.put("/edit/:id", (req, res) => {
+      var oldValue = {
+        _id: "req.param.id",
+      };
+      db.collection("question")
+        .updateMany(oldValue, req.body)
+        .then((results) => {
+          id = req.params.id;
+          res.render("update.ejs", { question: results });
+        })
+        .catch((error) => console.error(error));
+    });
 
     // ! DELETE REQUEST
 
     // ! static files
-    app.use(express.static(__dirname + '/public'));
-    })
-    .catch(console.error)
+    app.use(express.static(__dirname + "/public"));
+  })
+  .catch(console.error);
 
-app.listen(3000,()=> {
-    console.log('listening on 3000')
-})
-
-
-// todo https://zellwk.com/blog/crud-express-mongodb/
+app.listen(3000, () => {
+  console.log("listening on 3000");
+});
